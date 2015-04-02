2f3470d31a6dbd14ee891fd9217e9beb62c8b06a
Santiago <s.beltran10@uniandes.edu.co>
2015-04-01 23:34:26 -0500

    Constructor y atributos

--- proyectoInfracompCaso2/src/caso2/Cliente.java
+++ proyectoInfracompCaso2/src/caso2/Cliente.java
@@ -1,9 +1,14 @@
 package caso2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Cliente {
 	
 	public final static String SERV = "infracomp.virtual.uniandes.edu.co";
-	public final static String PORT = "443";
+	public final static int PORT = 443;
 	
 	public final static String HOLA = "HOLA";
 	public final static String INICIO ="INICIO";
@@ -29,4 +34,38 @@ public class Cliente {
 	private final static String HMACMD5 = "HMACMD5";
 	private final static String HMACSHA1 = "HMACSHA1";
 	private final static String HMACSHA256 = "HMACSHA256";
+	
+	//Atributos
+	
+	/**
+	 * El socket para realizar la conexion al servidor
+	 */
+	private Socket socket;
+	
+	/**
+	 * Writer par aenviar mensajes de control al servidor
+	 */
+	private PrintWriter out;
+	
+	/**
+	 * Reader para leer los mensajes de control enviados por el servidor
+	 */
+	private BufferedReader in;
+	
+	/**
+	 * Reader para visualizar como se lleva a cabo el protocolo
+	 */
+	private BufferedReader sysIn;
+	//Constructor
+	public Cliente() throws Exception{
+		try{
+			
+			socket = new Socket(SERV, PORT);
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			sysIn = new BufferedReader(new InputStreamReader(System.in));
+			
+		} catch(Exception e){ e.printStackTrace();}
+	}
+	
 }

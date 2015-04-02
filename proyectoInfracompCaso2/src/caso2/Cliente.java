4153a59b0f0d6a5afb1a23d7b76ca3c4c8a508c3
JairoBm13 <j.bautista.m13@outlook.com>
2015-04-01 23:03:18 -0500

    Protocolo

--- proyectoInfracompCaso2/src/caso2/Cliente.java
+++ proyectoInfracompCaso2/src/caso2/Cliente.java
@@ -1,5 +1,18 @@
 package caso2;
 
 public class Cliente {
+	
+	public final static String SERV = "infracomp.virtual.uniandes.edu.co";
+	public final static String PORT = "443";
+	
+	public final static String HOLA = "HOLA";
+	public final static String INICIO ="INICIO";
+	public final static String ALGORITMOS = "ALGORITMOS";
+	public final static String ESTADO = "ESTADO";
+	public final static String OK = "OK";
+	public final static String ERROR = "ERROR";
+	public final static String CERCLNT = "CERCLNT";
+	public final static String CERTSRV = "CERTSRV";
+	public final static String INIT = "INIT";
 
 }

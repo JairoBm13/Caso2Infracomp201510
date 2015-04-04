8777d7b2589009ee2e20ba581a375f8583a4f122
JairoBm13 <j.bautista.m13@outlook.com>
2015-04-03 20:01:04 -0500

    Establecer conexión y mandar algoritmos listos.

--- proyectoInfracompCaso2/src/caso2/Cliente.java
+++ proyectoInfracompCaso2/src/caso2/Cliente.java
@@ -1,86 +1,88 @@
 package caso2;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Cliente implements ICliente{
+public class Cliente implements ICliente {
 
 	/**
 	 * Direccion del servidor a usar.
 	 */
-	public final static String SERV = "infracomp.virtual.uniandes.edu.co";
+	private final static String SERV = "infracomp.virtual.uniandes.edu.co";
 	/**
 	 * Puerto servidor con seguridad
 	 */
-	public final static int PORT = 443;
+	private final static int PORT = 443;
 
 	/**
 	 * Puerto del servidor sin autenticaciones de seguridad
 	 */
-	public final static int PORTINSEGUR = 80;
+	private final static int PORTINSEGUR = 80;
 
 	/**
 	 * Cadena de control que indica el inicio de la conversacion
 	 */
-	public final static String HOLA = "HOLA";
+	private final static String HOLA = "HOLA";
 	/**
-	 * Cadena de control que indica el inicio de la conversacion por parte del servidor
+	 * Cadena de control que indica el inicio de la conversacion por parte del
+	 * servidor
 	 */
-	public final static String INICIO ="INICIO";
+	private final static String INICIO = "INICIO";
 
 	/**
-	 * Cadena de control que indica 
+	 * Cadena de control que indica
 	 */
-	public final static String ALGORITMOS = "ALGORITMOS";
+	private final static String ALGORITMOS = "ALGORITMOS";
 
 	/**
 	 * Cadena de control que indica el estado de la conexion
 	 */
-	public final static String ESTADO = "ESTADO";
+	private final static String ESTADO = "ESTADO";
 
 	/**
 	 * Cadena de control que indica una conexion exitosa
 	 */
-	public final static String OK = "OK";
+	private final static String OK = "OK";
 
 	/**
 	 * Cadena de control que indica una conexion fallida
 	 */
-	public final static String ERROR = "ERROR";
+	private final static String ERROR = "ERROR";
 
 	/**
 	 * Cadena de control que indica el envio del certificado del cliente
 	 */
-	public final static String CERCLNT = "CERCLNT";
+	private final static String CERCLNT = "CERCLNT";
 
 	/**
 	 * Cadena de control que indica el envio del certificado del servidor
 	 */
-	public final static String CERTSRV = "CERTSRV";
+	private final static String CERTSRV = "CERTSRV";
 
 	/**
 	 * Cadena de control que indica el inicio de seguridad.
 	 */
-	public final static String INIT = "INIT";
+	private final static String INIT = "INIT";
 
-	//Algoritmos para tareas de cifrado
+	// Algoritmos para tareas de cifrado
 
-	//Simetricos
+	// Simetricos
 	private final static String DES = "DES";
 	private final static String AES = "AES";
 	private final static String Blowfish = "Blowsfish";
 
-	//Asimetrico
+	// Asimetrico
 	private final static String RSA = "RSA";
 
-	//DOHASH
+	// DOHASH
 	private final static String HMACMD5 = "HMACMD5";
 	private final static String HMACSHA1 = "HMACSHA1";
 	private final static String HMACSHA256 = "HMACSHA256";
 
-	//Atributos
+	// Atributos
 
 	/**
 	 * El socket para realizar la conexion al servidor
@@ -101,40 +103,59 @@ public class Cliente implements ICliente{
 	 * Reader para visualizar como se lleva a cabo el protocolo
 	 */
 	private BufferedReader sysIn;
-	//Constructor
-	public Cliente() throws Exception{
-		try{
 
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	public Cliente() throws Exception {
+		try {
 			socket = new Socket(SERV, PORT);
 			out = new PrintWriter(socket.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			in = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
 			sysIn = new BufferedReader(new InputStreamReader(System.in));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-		} catch(Exception e){ e.printStackTrace();}
+	/**
+	 * 
+	 */
+	public boolean establecerConexion() throws IOException {
+		out.write(HOLA);
+		out.flush();
+		String respuesta = in.readLine();
+		if(respuesta.equals(INICIO)){
+			return true;
+		}
+		return false;
 	}
 
 	@Override
-	public boolean establecerConexion() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean mandarAlgoritmos(String algos, String algoa, String algod) throws IOException {
+		out.write(ALGORITMOS+":"+algos+":"+RSA+":"+algod);
+		out.flush();
+		String respuesta = in.readLine();
+		String estado = respuesta.split(":")[1];
+		if(estado.equals(OK)){return true;}
+		else{return false;}
 	}
-	@Override
-	public boolean mandarAlgoritmos() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+
 	@Override
 	public byte[] envioCertificado() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public boolean actualizarUbicacion() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
 	}
 }
--- proyectoInfracompCaso2/src/caso2/ICliente.java
+++ proyectoInfracompCaso2/src/caso2/ICliente.java
@@ -1,10 +1,12 @@
 package caso2;
 
+import java.io.IOException;
+
 public interface ICliente {
 
-	public boolean establecerConexion();
+	public boolean establecerConexion() throws IOException;
 	
-	public boolean mandarAlgoritmos();
+	public boolean mandarAlgoritmos(String algos, String algoa, String algod) throws IOException;
 	
 	public byte[] envioCertificado();
 	

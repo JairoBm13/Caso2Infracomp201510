8a9bb702ac0f36a7107c83ef675a96b219dccbf1
JairoBm13 <j.bautista.m13@outlook.com>
2015-04-03 21:17:25 -0500

    Certificado

--- proyectoInfracompCaso2/src/caso2/Cliente.java
+++ proyectoInfracompCaso2/src/caso2/Cliente.java
@@ -4,15 +4,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.math.BigInteger;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.SignatureException;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
+import java.util.Date;
 
+import javax.security.auth.x500.X500Principal;
+
+import org.bouncycastle.x509.X509V1CertificateGenerator;
+import org.bouncycastle.x509.X509V3CertificateGenerator;
+
+/**
+ * 
+ * @author Jairo Bautista & Santiago Beltran Caicedo
+ *
+ */
 public class Cliente implements ICliente {
 
-	//Constantes
+	//------------------------------------------------
+	// Constantes
+	//------------------------------------------------
+	
 	/**
 	 * Direccion del servidor a usar.
 	 */
 	private final static String SERV = "infracomp.virtual.uniandes.edu.co";
+	
 	/**
 	 * Puerto servidor con seguridad
 	 */
@@ -68,22 +93,58 @@ public class Cliente implements ICliente {
 	 */
 	private final static String INIT = "INIT";
 
-	// Algoritmos para tareas de cifrado
+	/* Algoritmos para tareas de cifrado*/
 
+	//--------------------------------------------
 	// Simetricos
+	//--------------------------------------------
+	
+	/**
+	 * 
+	 */
 	private final static String DES = "DES";
+	
+	/**
+	 * 
+	 */
 	private final static String AES = "AES";
+	
+	/**
+	 * 
+	 */
 	private final static String Blowfish = "Blowsfish";
 
+	//-------------------------------------------------
 	// Asimetrico
+	//-------------------------------------------------
+	
+	/**
+	 * 
+	 */
 	private final static String RSA = "RSA";
 
+	//-------------------------------------------------
 	// DOHASH
+	//-------------------------------------------------
+	
+	/**
+	 * 
+	 */
 	private final static String HMACMD5 = "HMACMD5";
+	
+	/**
+	 * 
+	 */
 	private final static String HMACSHA1 = "HMACSHA1";
+	
+	/**
+	 * 
+	 */
 	private final static String HMACSHA256 = "HMACSHA256";
 
+	//-------------------------------------------------
 	// Atributos
+	//-------------------------------------------------
 
 	/**
 	 * El socket para realizar la conexion al servidor
@@ -105,7 +166,15 @@ public class Cliente implements ICliente {
 	 */
 	private BufferedReader sysIn;
 
-	//Constructor
+	//-----------------------------------------------
+	// Constructor
+	//-----------------------------------------------
+	
+	/**
+	 * 
+	 * @param port
+	 * @throws Exception
+	 */
 	public Cliente(int port) throws Exception{
 		try{
 
@@ -137,26 +206,97 @@ public class Cliente implements ICliente {
 	 * 
 	 */
 	public boolean mandarAlgoritmos(String algos, String algoa, String algod) throws IOException {
+
 		System.out.println("Cliente: " + ALGORITMOS+":"+algos+":"+RSA+":"+algod);
 		out.println(ALGORITMOS+":"+algos+":"+RSA+":"+algod);
+
 		String respuesta = in.readLine();
+
 		System.out.println("Servidor: " + respuesta);
 		String estado = respuesta.split(":")[1];
+
 		if(estado.equals(OK)){return true;}
 		else{return false;}
 	}
 
-	@Override
+	/**
+	 * 
+	 */
 	public byte[] envioCertificado() {
-		// TODO Auto-generated method stub
-		return null;
+		X509Certificate certificado = crearCertificado();
+		byte[] certByte;
+		try {
+			certByte = certificado.getEncoded();
+			out.println(CERCLNT);
+			System.out.println("Cliente: "+CERCLNT);
+			System.out.println(certByte);
+			socket.getOutputStream().write(certByte);
+			socket.getOutputStream().flush();	
+			return certByte;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
-	@Override
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private X509Certificate crearCertificado(){
+		Date startDate = new Date(System.currentTimeMillis());                
+		Date expiryDate = new Date(System.currentTimeMillis() + 30L * 365L * 24L * 60L * 60L * 1000L);
+		BigInteger serialNumber = new BigInteger("26");       
+		
+		KeyPair parDeLlaves = generarLlave();
+		PrivateKey caKey = parDeLlaves.getPrivate();              
+		X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
+		X500Principal  subjectName = new X500Principal("CN=Test V3 Certificate"); 
+		certGen.setSerialNumber(serialNumber);
+		certGen.setIssuerDN(subjectName);
+		certGen.setNotBefore(startDate);
+		certGen.setNotAfter(expiryDate);
+		certGen.setSubjectDN(subjectName);
+		certGen.setPublicKey(parDeLlaves.getPublic());
+		certGen.setSignatureAlgorithm("RSA"); 
+		try {
+			X509Certificate cert = certGen.generate(caKey, "BC"); 
+			return cert;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}   
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private KeyPair generarLlave(){		
+		KeyPairGenerator generator;
+		try {
+			generator = KeyPairGenerator.getInstance(RSA);
+			generator.initialize(1024);
+			KeyPair keyPair = generator.generateKeyPair();
+			return keyPair;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * 
+	 */
 	public boolean actualizarUbicacion() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args){
 		Cliente cli = null;
 		try{
@@ -171,5 +311,6 @@ public class Cliente implements ICliente {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		cli.envioCertificado();
 	}
 }
